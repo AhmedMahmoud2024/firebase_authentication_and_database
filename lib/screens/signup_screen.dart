510a41b6068c10b9732c8f4a99a29edf3ab968c4
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2026-06-16 03:48:10 -0700

    feat:implemented signup logic mapping

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_authentication_and_database/services/firebase_auth_service.dart';
+import 'package:firebase_authentication_and_database/services/firestore_service.dart';
 import 'package:flutter/material.dart';
 
 class SignupScreen extends StatefulWidget {
@@ -8,8 +10,53 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
- 
- 
+ final TextEditingController _nameController= TextEditingController();
+  final TextEditingController _emailController= TextEditingController();
+   final TextEditingController _passwordController= TextEditingController();
+  final _authService=FirebaseAuthService();
+  final _firestoreServices=FirestoreService();
+  bool _isLoading=false;
+
+void signUp()async{
+if(_nameController.text.isEmpty || _emailController.text.isEmpty || _passwordController.text.isEmpty){
+  ScaffoldMessenger.of(context).showSnackBar(
+    const SnackBar(content: Text('Please fill in all fields'),)
+  );
+    return;
+}
+setState(() => _isLoading=true,);
+try{
+final credrntial= await _authService.singUpWithEmailAndPassword(
+    _emailController.text.trim(),
+     _passwordController.text.trim()
+     );
+     if(credrntial.user !=null){
+      _firestoreServices.saveUserData(
+        uid: credrntial.user!.uid,
+         name: _nameController.text.trim(),
+          email: _emailController.text.trim()
+          );
+          if(mounted){
+            Navigator.pushReplacement(
+              context,
+              //Homepage
+              MaterialPageRoute(builder: (context) =>  Container())
+            );
+          }
+     }
+}catch(e){
+   if(mounted){
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(e.toString().replaceAll('Exception: ', '')),)
+    );
+   }
+}finally{
+  if(mounted){
+    setState(() => _isLoading=false,);
+  }
+}
+}
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
